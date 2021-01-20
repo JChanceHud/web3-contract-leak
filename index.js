050f6c9f95ea806ea071b6aa86af5055a09d360e
Chance Hudson <jchancehud@gmail.com>
2021-01-20 00:55:22 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const heapdump = require('heapdump')
+const path = require('path')
+const Web3 = require('web3')
+const Eth = require('web3-eth')
+const Contract = require('web3-eth-contract')
+
+const mainnet = 'wss://192.168.1.199:8546'
+const web3 = new Web3(mainnet)
+
+const addr = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+const ABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]
+
+let lastAmount = 0
+function printMemory() {
+  if (global.gc) {
+    global.gc()
+  }
+  const used = process.memoryUsage().heapUsed / 1024 / 1024
+  if (used < lastAmount && process.argv.indexOf('--dump') !== -1) {
+    heapdump.writeSnapshot(path.join(__dirname, `${+new Date()}.heapsnapshot`))
+  }
+  lastAmount = used
+  console.log(`Heap used: ${used} MB`)
+}
+
+;(async () => {
+  //for (let x = 0; x < 20000; x++) {
+  //  if (x % 1000 === 0) printMemory()
+  //  const eth = new Eth()
+  //  const contract = new eth.Contract(ABI, addr)
+  //}
+  for (let x = 0; x < 20000; x++) {
+    if (x % 1000 === 0) printMemory()
+    const contract = new web3.eth.Contract(ABI, addr)
+  }
+})()
